2b9d544ae45679636d237061b6a6ee644ec065ab
Ivaylo1992 <ivailop123@gmail.com>
2025-03-21 06:43:39 +0200

    feat: added get_headers endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from typing import Optional
 
 from pydantic import BaseModel
@@ -9,6 +9,7 @@ app = FastAPI()
 async def read_root():
     return {'message': 'Welcome !'}
 
+
 @app.get('/greet')
 async def greet_name(name:Optional[str] = 'User', age:int = 0) -> dict:
     return {'message': f'Hello {name}', 'age': age}
@@ -18,6 +19,25 @@ class BookCreateModel(BaseModel):
     title: str
     author: str
 
+
 @app.post('/create_book')
 async def create_book(book_data: BookCreateModel) -> dict:
     return {'title': book_data.title, 'author': book_data.author}
+
+
+@app.get('/get_headers', status_code=200)
+async def get_headers(
+    accept:str = Header(None),
+    content_type:str = Header(None),
+    user_agent:str = Header(None),
+    host:str = Header(None) 
+)-> dict:
+    
+    request_headers = {}
+
+    request_headers['Accept'] = accept
+    request_headers['Content-Type'] = content_type
+    request_headers['User-Agent'] = user_agent
+    request_headers['Host'] = host
+
+    return request_headers
