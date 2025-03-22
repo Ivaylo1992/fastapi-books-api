69c881c72b5b9a745849227b4ae0802648d48b03
Ivaylo1992 <ivailop123@gmail.com>
2025-03-22 05:53:13 +0200

    feat: created BookModel and get_all_books endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Header
-from typing import Optional
+from typing import Optional, List
 
 from pydantic import BaseModel
 
@@ -15,23 +15,13 @@ async def greet_name(name:Optional[str] = 'User', age:int = 0) -> dict:
     return {'message': f'Hello {name}', 'age': age}
 
 
-class BookCreateModel(BaseModel):
-    title: str
-    author: str
-
-
-@app.post('/create_book')
-async def create_book(book_data: BookCreateModel) -> dict:
-    return {'title': book_data.title, 'author': book_data.author}
-
-
 @app.get('/get_headers', status_code=200)
 async def get_headers(
     accept:str = Header(None),
     content_type:str = Header(None),
     user_agent:str = Header(None),
     host:str = Header(None) 
-)-> dict:
+) -> dict:
     
     request_headers = {}
 
@@ -41,3 +31,76 @@ async def get_headers(
     request_headers['Host'] = host
 
     return request_headers
+
+
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "Antonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "published_date": "2022-01-19",
+        "page_count": 1023,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "published_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "Oreilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 9282,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-21-01",
+        "page_count": 3006,
+        "language": "English",
+    },
+]
+
+
+class BookModel(BaseModel):
+        id: int
+        title: str
+        author: str
+        publisher: str
+        published_date: str
+        page_count: int
+        language: str 
+
+
+@app.get('/books', response_model=List[BookModel])
+async def get_all_books():
+    return books
