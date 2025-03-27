b80cdda03cfe384acc11d865f3bb4faf78210b46
Ivaylo1992 <ivailop123@gmail.com>
2025-03-27 05:43:21 +0200

    feat: connected to postgresql

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from .books.routers import book_router
+from .db.main import init_db
+
+async def life_span(app: FastAPI):
+    print('The server is starting ...')
+    await init_db()
+    yield
+    print('The server has been stopped.')
+
 
 version = 'v1'
 
 app = FastAPI(
     version=version,
     title='BooksPy',
-    description='A REST API for a book review web service.'
+    description='A REST API for a book review web service.',
+    lifespan=life_span
 )
 
 app.include_router(book_router, prefix='/api/{version}/books', tags=['books'])
