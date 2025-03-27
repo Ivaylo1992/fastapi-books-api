015436a9b6ea5ac67a7af978c66a7fcfaa6e4897
Ivaylo1992 <ivailop123@gmail.com>
2025-03-27 06:11:07 +0200

    feat: added Books sql model

---
+++ src/books/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+
+class Book(SQLModel, table=True):
+    __tablename__ = 'books'
+
+    uid: UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid4
+        )
+    )
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+    created_at: datetime = Field(
+        Column(
+            pg.TIMESTAMP,
+            default=datetime.now
+        )
+    )
+    updated_at: datetime = Field(
+        Column(
+            pg.TIMESTAMP,
+            default=datetime.now
+        )
+    )
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
